d6b543fdefdf7207400f6edc9bd18c8a3df850b6
Sultonov557 <sultonovzerifboy@gmail.com>
2024-01-21 18:40:02 +0500

    .

--- cli.js
+++ cli.js
@@ -1,15 +1,21 @@
 const lisa = require("./index");
 
 const readline = require("node:readline").createInterface({
-  input: process.stdin,
-  output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 });
 
 run();
 function run() {
-  readline.question(`You: `, (input) => {
-    const output = lisa.correct(input);
-    console.log("Lisa:", output);
-    run();
-  });
+	readline.question(`You: `, (input) => {
+		const inputs = input.split(" ");
+		let answer = "";
+		for (let input of inputs) {
+			const output = lisa.correct(input, 0.75);
+			answer += output.words[0] + " ";
+		}
+
+		console.log(answer);
+		run();
+	});
 }
--- textParse.js
+++ textParse.js
@@ -3,13 +3,28 @@ const fs = require("fs");
 const readStream = fs.createReadStream(__dirname + "/data1.txt");
 const writeStream = fs.createWriteStream(__dirname + "/data.txt");
 
+let data = [];
+let wordLimit = 1000000;
+
 readStream.on("data", (chunk) => {
-  chunk = chunk
-    .toLocaleString()
-    .toLocaleLowerCase()
-    .replace(/[^a-z ]/g, "")
-    .split(" ");
-  chunk = [...new Set(chunk)];
-  chunk = chunk.join(" ");
-  writeStream.write(chunk);
+	data = data.concat(
+		chunk
+			.toLocaleString()
+			.toLocaleLowerCase()
+			.replace(/[^a-z ]/g, "")
+			.split(" ")
+	);
+	data = [...new Set(data)];
+	console.log(`${data.length}/${wordLimit}`);
+	if (data.length > wordLimit) {
+		end();
+		process.abort();
+	}
 });
+
+readStream.on("end", end);
+
+function end() {
+	data = data.join(" ");
+	writeStream.write(data);
+}
